e1a0c352a3cbb9bb4a0f111eee9f3c7ee7d88cee
May Faraj <mayfaraj2001@gmail.com>
2026-06-24 20:18:56 +0300

    task-8: add saved jobs with localStorage

--- src/main.js
+++ src/main.js
@@ -1,16 +1,38 @@
 const jobsContainer = document.getElementById("jobsContainer");
 const totalJobsElement = document.getElementById("totalJobs");
+const savedJobsElement = document.getElementById("savedJobs");
+const appliedJobsElement = document.getElementById("appliedJobs");
+const interviewJobsElement = document.getElementById("interviewJobs");
 
 const searchInput = document.getElementById("searchInput");
 const categoryFilter = document.getElementById("categoryFilter");
 const typeFilter = document.getElementById("typeFilter");
 
+function getSavedJobs() {
+  return JSON.parse(localStorage.getItem("savedJobs")) || [];
+}
+
+function saveJob(jobId) {
+  const savedJobs = getSavedJobs();
+
+  if (!savedJobs.includes(jobId)) {
+    savedJobs.push(jobId);
+    localStorage.setItem("savedJobs", JSON.stringify(savedJobs));
+  }
+
+  updateDashboard();
+  filterJobs();
+}
+
 function renderJobs(filteredJobs = jobs) {
+  const savedJobs = getSavedJobs();
+
   jobsContainer.innerHTML = "";
 
   filteredJobs.forEach(job => {
-    const jobCard = document.createElement("div");
+    const isSaved = savedJobs.includes(job.id);
 
+    const jobCard = document.createElement("div");
     jobCard.classList.add("job-card");
 
     jobCard.innerHTML = `
@@ -23,7 +45,9 @@ function renderJobs(filteredJobs = jobs) {
 
       <p>${job.description}</p>
 
-      <button>Save Job</button>
+      <button onclick="saveJob(${job.id})">
+        ${isSaved ? "Saved" : "Save Job"}
+      </button>
     `;
 
     jobsContainer.appendChild(jobCard);
@@ -40,15 +64,14 @@ function filterJobs() {
   const filteredJobs = jobs.filter(job => {
     const matchesSearch =
       job.title.toLowerCase().includes(searchText) ||
-      job.company.toLowerCase().includes(searchText);
+      job.company.toLowerCase().includes(searchText) ||
+      job.description.toLowerCase().includes(searchText);
 
     const matchesCategory =
-      selectedCategory === "all" ||
-      job.category === selectedCategory;
+      selectedCategory === "all" || job.category === selectedCategory;
 
     const matchesType =
-      selectedType === "all" ||
-      job.type === selectedType;
+      selectedType === "all" || job.type === selectedType;
 
     return matchesSearch && matchesCategory && matchesType;
   });
@@ -56,8 +79,17 @@ function filterJobs() {
   renderJobs(filteredJobs);
 }
 
+function updateDashboard() {
+  const savedJobs = getSavedJobs();
+
+  savedJobsElement.textContent = savedJobs.length;
+  appliedJobsElement.textContent = 0;
+  interviewJobsElement.textContent = 0;
+}
+
 searchInput.addEventListener("input", filterJobs);
 categoryFilter.addEventListener("change", filterJobs);
 typeFilter.addEventListener("change", filterJobs);
 
+updateDashboard();
 renderJobs();
